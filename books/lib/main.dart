1b696afef4a104fdb03ce58b45db94c562dba07e
ririks <ririkomalasari870@gmail.com>
2025-03-21 10:15:28 +0700

    upload praktikum 6

--- books/lib/main.dart
+++ books/lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:books/geolocation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:async/async.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const FuturePage(),
+       home: LocationScreen(),
     );
   }
 }
