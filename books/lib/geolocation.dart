62af0ad33a39fa5775929fcaae78cd3646ba54c7
ririks <ririkomalasari870@gmail.com>
2025-03-21 10:49:07 +0700

    upload praktikum 7

--- books/lib/geolocation.dart
+++ books/lib/geolocation.dart
@@ -9,36 +9,69 @@ class LocationScreen extends StatefulWidget {
 }
 
 class _LocationScreenState extends State<LocationScreen> {
-  String myPosition = '';
+Future<Position>? position;
 
-  @override
+@override
   void initState() {
     super.initState();
-    getPosition().then((Position myPos) {
-      myPosition =
-          'Latitude: ${myPos.latitude.toString()} - Longitude: ${myPos.longitude.toString()}';
-      setState(() {
-        myPosition = myPosition;
-      });
-    });
+    position = getPosition();
   }
+  //String myPosition = '';
+
+  //@override
+  //void initState() {
+   // super.initState();
+    //getPosition().then((Position myPos) {
+      //myPosition =
+          //'Latitude: ${myPos.latitude.toString()} - Longitude: ${myPos.longitude.toString()}';
+      //setState(() {
+       // myPosition = myPosition;
+      //});
+    //});
+ // }
+
+  //@override
+  //Widget build(BuildContext context) {
+    //final myWidget = myPosition == ''
+        //? const CircularProgressIndicator()
+        // Text(myPosition);
+
+    //return Scaffold(
+      //appBar: AppBar(title: const Text('Riri Komalasari')),
+      //body: Center(child: myWidget),
+    //);
+  //}
 
   @override
   Widget build(BuildContext context) {
-    final myWidget = myPosition == ''
-        ? const CircularProgressIndicator()
-        : Text(myPosition);
-
     return Scaffold(
       appBar: AppBar(title: const Text('Riri Komalasari')),
-      body: Center(child: myWidget),
+      body: Center(
+        child: FutureBuilder(
+          future: position,
+          builder: (BuildContext context, AsyncSnapshot<Position> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } 
+              else if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasError) {
+              return Text('Something terrible happened!');
+                  }
+              return Text(snapshot.data.toString());
+            }
+            else {
+              return const Text('');
+            } 
+          },
+        ),
+      ),
     );
   }
 
   Future<Position> getPosition() async {
-    await Geolocator.requestPermission();
     await Geolocator.isLocationServiceEnabled();
-    Position? position = await Geolocator.getCurrentPosition();
+    await Future.delayed(const Duration(seconds: 3)); 
+    Position position = await Geolocator.getCurrentPosition();
     return position;
   }
 }
